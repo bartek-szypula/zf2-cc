2210d985ea0a199902be832255f4af620ad2396e
Bartlomiej Szypula <bartlomiej.szypula@codeconcept.pl>
2013-05-24 16:33:44 +0200

    'todo'

--- module/Auth/config/module.config.php
+++ module/Auth/config/module.config.php
@@ -1,3 +1,13 @@
 <?php
 return array(
+	'controllers' => array(
+        'invokables' => array(
+            'Auth\Controller\Auth' => 'Auth\Controller\AuthController',
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'album' => __DIR__ . '/../view',
+        ),
+    ),
 );
